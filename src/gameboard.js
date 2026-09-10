e7b797613aa36f79ecc0243193799a7d048fa739
Victor Mukumbu | Software Developer <57131903+VictorMukumbu@users.noreply.github.com>
2026-09-10 10:11:32 +0300

    test: register ship hits on gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,5 +1,28 @@
+import { Ship } from "./ship"
+
 export function Gameboard(){
+    function placeShip(ship,coordinates){
+        let expectedShips ={
+            ship:ship,
+            coordinates:coordinates,
+        } 
+        return this.ships.push(expectedShips)
+    }
+    function receiveAttack(coordinate) {
+        for (const ship of this.ships){           
+            let targetStr = coordinate.join(',');
+
+            let hasMatch = ship.coordinates.some(coordinate => coordinate.join(',') === targetStr);
+
+            if(hasMatch){
+                return ship.ship.hit()
+            }            
+        }    
+        return  
+    }
     return{
-        ships:[]
+        ships:[],
+        placeShip,
+        receiveAttack,
     }
 }
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,6 +1,31 @@
 import { Gameboard } from "../src/gameboard.js";
+import { Ship } from "../src/ship.js";
 
 test('Gameboard() actually creates a board',()=>{
     let board = Gameboard()
     expect(board.ships).toEqual([])
+})
+test('Gameboard should be able to place ships at specific coordinates',()=>{
+    let board =Gameboard()
+    let ship =Ship(3)
+    let coordinates = [[0, 0], [0, 1], [0, 2]]
+    let expectedShips ={
+        ship:ship,
+        coordinates:coordinates
+    }
+    board.placeShip(ship, coordinates)
+    expect(board.ships.length).toBe(1)
+    expect(board.ships[0]).toEqual(expectedShips)
+})
+
+test('Gameboard should be able to receive an attack  at a coordinate and determine whether a ship was hit',()=>{
+    let ship =Ship(3)
+    let board =Gameboard()
+    let coordinates = [[0, 0], [0, 1], [0, 2]]
+
+    board.placeShip(ship,coordinates) //placeShip on board
+    
+    board.receiveAttack(coordinates[0]) //attackboard
+
+    expect(ship.hits).toBe(1)
 })
