cbbb4d6562bb63949965e0f0904814adbb4a1efb
Victor Mukumbu | Software Developer <57131903+VictorMukumbu@users.noreply.github.com>
2026-09-18 19:43:41 +0300

    feat: expose player and opponent boards in game ui

--- src/game-ui.js
+++ src/game-ui.js
@@ -2,6 +2,12 @@ import {GameController} from "./game-controller.js"
 
 export function GameUi(){
     let gameController = GameController()
+    let playerBoard = gameController.player1.board
+    let opponentBoard = gameController.player2.board
 
-    return {gameController}
+    return {
+        gameController,
+        playerBoard,
+        opponentBoard,
+    }
 }
--- tests/game-ui.test.js
+++ tests/game-ui.test.js
@@ -2,3 +2,7 @@ import { GameUi } from "../src/game-ui"
 test(`calling GameUi accesses a GameController`,()=>{
     expect(Object.hasOwn(GameUi(),"gameController")).toBe(true)
 })
+test(`GameUi() provides access to both boards.`,()=>{
+    expect(Object.hasOwn(GameUi(),"playerBoard")).toBe(true)
+    expect(Object.hasOwn(GameUi(),"opponentBoard")).toBe(true)
+})
